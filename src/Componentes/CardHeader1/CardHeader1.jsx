69da4659613e577cda2a6d6812a3ef6706138bb6
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-06-04 23:41:17 -0300

    update

--- src/Componentes/CardHeader1/CardHeader1.jsx
+++ src/Componentes/CardHeader1/CardHeader1.jsx
@@ -3,7 +3,11 @@ import "./CardHeader1.css";
 const CardHeader1 = (props) => {
   return (
     <div className="container-card-header-2">
-      <img src={props.Sapatella} alt="Logo da Sapatella" />
+      <img
+        className="container-img-card-header-2"
+        src={props.Sapatella}
+        alt="Logo da Sapatella"
+      />
     </div>
   );
 };
--- src/Componentes/HeaderMobile/HeaderMobile.jsx
+++ src/Componentes/HeaderMobile/HeaderMobile.jsx
@@ -1,30 +1,56 @@
+import { useEffect, useState } from "react";
 import CardHeader1 from "../CardHeader1/CardHeader1";
 import CardHeader3Mobile from "../CardHeader3Mobile/CardHeader3Mobile";
 import DataHeader from "../DataHeader/DataHeader";
 import MenuLateral from "../MenuLateral/MenuLateral";
 import "./HeaderMobile.css";
+import UseDrawerContext from "../../Hook/UseDrawerContext";
 
 const HeaderMobile = () => {
-  return (
-    <section className="container-header-mobile">
-      <div>
-        <MenuLateral />
-      </div>
-      <div className="container-header-mobile-card-1">
-        {DataHeader.map((props) => (
-          <CardHeader1 key={props.id} Sapatella={props.Sapatella} />
-        ))}
-      </div>
+  const [scrolled, setScroulled] = useState(false);
 
-      <div className="container-header-mobile-card-3">
-        {DataHeader.map((props) => (
-          <CardHeader3Mobile
-            key={props.id}
-            Search={props.Search}
-            Puppet={props.Puppet}
-            MiniCart={props.MiniCart}
-          />
-        ))}
+  const { Open } = UseDrawerContext();
+
+  useEffect(() => {
+    const handleScroull = () => {
+      if (window.scrollY > 0) {
+        setScroulled(true);
+      } else {
+        setScroulled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroull);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroull);
+    };
+  }, []);
+  return (
+    <section className={Open ? "actived" : "not-actived"}>
+      <div
+        className={
+          scrolled ? "container-header-mobile-fixo" : "container-header-mobile"
+        }
+      >
+        <div>
+          <MenuLateral />
+        </div>
+        <div className="container-header-mobile-card-1">
+          {DataHeader.map((props) => (
+            <CardHeader1 key={props.id} Sapatella={props.Sapatella} />
+          ))}
+        </div>
+        <div className="container-header-mobile-card-3">
+          {DataHeader.map((props) => (
+            <CardHeader3Mobile
+              key={props.id}
+              Search={props.Search}
+              Puppet={props.Puppet}
+              MiniCart={props.MiniCart}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
